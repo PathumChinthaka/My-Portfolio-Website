5cba85be82d3979c08e1f6c0627d790effa9b78f
pathum-chinthaka <104442327+pathum-chinthaka@users.noreply.github.com>
2023-07-09 22:19:37 +0530

    add about-me content details

--- Asset/Js/index.js
+++ Asset/Js/index.js
@@ -2,4 +2,18 @@
 // add js about content view
 
 var subLinks=document.getElementsByClassName('sub-links');
-var subContent=document.getElementsByClassName('sub-content');
+var subContents=document.getElementsByClassName('sub-content');
+
+function openTab(tabname){
+    for(subLink of subLinks){
+        subLink.classList.remove('active-link');
+    }
+
+    for(subContent of subContents){
+        subContent.classList.remove('active-content');
+    }
+
+    event.currentTarget.classList.add('active-link');
+
+    document.getElementById(tabname).classList.add('active-content');
+}
