c772376be95935f093f7b04fa1ee1f8a5eecdc2b
pathum-chinthaka <104442327+pathum-chinthaka@users.noreply.github.com>
2023-07-26 10:18:02 +0530

    preloader event

--- Asset/Js/index.js
+++ Asset/Js/index.js
@@ -1,8 +1,9 @@
-
-document.addEventListener("DOMContentLoaded", () => {
+// window on load method
+function windowLoad(){
     var preloader=document.getElementById('pre-loader');
     preloader.style="display:none";
-  });
+}
+
 // add js about content view
 var subLinks=document.getElementsByClassName('sub-links');
 var subContents=document.getElementsByClassName('sub-content');
